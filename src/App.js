0f4caf83c5f3772aa3bbdaa2b3978c3dd9f32b65
Deinmajones <deinmatuboyedia@gmail.com>
2023-11-15 22:34:31 +0100

    react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+//import Hello from './Components/Hello';
+//import Message from './Components/Message';
+import Profile from './Components/Profile';
+function App() {
+  return (
+    <div className="App">
+      <Profile name="Deinma" lastname="Jones"/>
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/Components/Hello.js
@@ -0,0 +1,10 @@
+const name = "Ashutosh";
+
+const displayMessage = () => {
+    return "I need help!"
+}
+function Hello(){
+    return <h1>This message is: {displayMessage()}</h1>
+}
+
+export default Hello;
